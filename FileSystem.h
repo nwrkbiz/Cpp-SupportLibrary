9b1d4bc5557f6e60d8b5bb0e1ca27dbeb1ca0616
Daniel G. (giri) <giri@nwrk.biz>
2020-02-03 12:12:32 +0100

    Add ExecuteSync function.

--- FileSystem.h
+++ FileSystem.h
@@ -101,6 +101,27 @@ namespace giri {
             }
             return "";
         }
+        /**
+         * Executes a command synchronously.
+         * @param cmd Command to execute.
+         * @returns The output of the command after finishing execution.
+         */
+        std::string ExecuteSync(const std::string& cmd) {
+            std::array<char, 128> buffer;
+            std::string result;
+        #if defined(WIN32)
+            std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd.c_str(), "r"), _pclose);
+        #else
+            std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
+        #endif
+            if (!pipe) {
+                throw FileSystemException("popen() failed!");
+            }
+            while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+                result += buffer.data();
+            }
+            return result;
+        }
     }
 }
 #endif //SUPPORTLIB_FILESYSTEM_H
--- docs/html/search/all_4.js
+++ docs/html/search/all_4.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['encode',['Encode',['../namespacegiri_1_1Base64.html#a49673d46cf5535fe22a659ea1f2e8bc4',1,'giri::Base64']]],
   ['exception_2eh',['Exception.h',['../Exception_8h.html',1,'']]],
-  ['exceptionbase',['ExceptionBase',['../classgiri_1_1ExceptionBase.html',1,'giri::ExceptionBase'],['../classgiri_1_1ExceptionBase.html#a04f8addab623e7119041bdf8d97b36b9',1,'giri::ExceptionBase::ExceptionBase()']]]
+  ['exceptionbase',['ExceptionBase',['../classgiri_1_1ExceptionBase.html',1,'giri::ExceptionBase'],['../classgiri_1_1ExceptionBase.html#a04f8addab623e7119041bdf8d97b36b9',1,'giri::ExceptionBase::ExceptionBase()']]],
+  ['executesync',['ExecuteSync',['../namespacegiri_1_1FileSystem.html#a59c8718235df6900917aa6be65f4a92c',1,'giri::FileSystem']]]
 ];
